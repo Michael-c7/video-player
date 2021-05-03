a003af80539b5101888b6f5ef854f4a258756cd9
Michael Carr <mcarr2468@gmail.com>
2021-05-03 18:44:57 -0500

    V1.0

---
+++ javascript/data/videos.js
@@ -0,0 +1,45 @@
+const videoList = [
+    {
+        id:1,
+        fileLocation: "../../videos/Hip_Hop.mp4",
+        title:"Hip_Hop",
+        author:"Deadmau5",
+        genre:"Music",
+        duration:"00:13",
+    },
+    {
+        id:2,
+        fileLocation: "../../videos/Island in the Wilderness (Season 29 Episode 1).mp4",
+        title:"Island in the Wilderness (Season 29 Episode 1)",
+        author:"Bob Ross",
+        genre:"Art",
+        duration:"25:09",
+    },
+    {
+        id:3,
+        fileLocation: "../../videos/Open House Night.mp4",
+        title:"Open House Night",
+        author:"Bill Hicks",
+        genre:"Comedy",
+        duration:"01:02",
+    },
+    {
+        id:4,
+        fileLocation: "../../videos/Shang-Chi and the Legend of the Ten Rings Official Teaser.mp4",
+        title:"Shang-Chi and the Legend of the Ten Rings Official Teaser",
+        author:"Marvel Studios",
+        genre:"Action",
+        duration:"02:05",
+    },
+    {
+        id:5,
+        fileLocation: "../../videos/Strike Me Down Zeus.mp4",
+        title:"Strike Me Down Zeus",
+        author:"Unknown",
+        genre:"Comedy",
+        duration:"00:12",
+    },
+]
+
+
+export default videoList;
