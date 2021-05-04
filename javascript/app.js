eeabea6d3309a291bd8f3330a7ffb9212fa8f16d
Michael Carr <mcarr2468@gmail.com>
2021-05-04 14:35:31 -0500

    V1.1

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,23 @@
 /*app uses the revealing module pattern*/
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
+
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+
+// updating range value
+https://stackoverflow.com/questions/19625334/range-slider-event-handler-javascript#19625696
+
+volume change event
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/volumechange_event
+
+// muted
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/muted
+
+// volume
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/volume
+
+
+// HTMLMediaElement
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+ */
